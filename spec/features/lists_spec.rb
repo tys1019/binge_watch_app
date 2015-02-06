6441e96cb388198dc382246060f4c5f2ddabc065
Daniel Tyson <danielctyson@gmail.com>
2015-02-06 16:14:36 -0500

    fixed bootstrap setup

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'Managing list' do
   scenario 'Add show to list' do
-    visit '/lists/1'
+
+    list = List.create!
+
+    breaking = Show.create!(name: 'Breaking Bad', artwork: 'http://thetvdb.com/banners/_cache/posters/81189-10.jpg', description: "Mild-mannered high school chemistry teacher Walter")
+
+    season = Season.create!(number: "1")
+    breaking.seasons << season
+
+    1.upto(5) do |i|
+       breaking.seasons.find(season.id).episodes << Episode.create!(number: i, length: 42)
+    end
+
+    visit "/lists/#{list.id}"
 
     click_on 'Add'
 
--- spec/features/shows_spec.rb
+++ spec/features/shows_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.feature 'Managing shows' do
   scenario 'Welcome page shows all the TV shows' do
+    Show.create!(name: 'Breaking Bad', artwork: 'http://thetvdb.com/banners/_cache/posters/81189-10.jpg', description: "Mild-.")
+
     visit '/'
 
-    expect(page).to have_selector 'section', count: 3
+    expect(page).to have_selector 'section'
   end
 
   scenario 'Show all TV shows and episodes' do
+    Show.create!(name: 'Breaking Bad', artwork: 'http://thetvdb.com/banners/_cache/posters/81189-10.jpg', description: "Mild-.")
+
     visit '/shows'
 
     expect(page).to have_selector 'section'
